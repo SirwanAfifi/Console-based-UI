382df569018401d27da84a4a0c77c19ac9272dbc
Sirwan Afifi <sirwan@Sirwans-MacBook-Pro.local>
2018-05-04 14:57:23 +0430

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using NStack;
+using Terminal.Gui;
+
+namespace TestingGuiCS.Models
+{
+    public class Movie
+    {
+        public double Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public MpaaRating MpaaRating { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | ReleaseDate: {ReleaseDate:dd/mm/yyyy} | MpaaRating: {MpaaRating} | Genre: {Genre} | Rating: {Rating}";
+        }
+    }
+
+    public enum MpaaRating
+    {
+        G = 1,
+        PG = 2,
+        PG13 = 3,
+        R = 4
+    }
+
+    public static class MovieDataSource
+    {
+        public static Movie GetOne(long id)
+        {
+            return GetList().FirstOrDefault(x => x.Id == id);
+        }
+        
+        public static IEnumerable<Movie> GetList()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "The Amazing Spider-Man", ReleaseDate = new DateTime(2012, 07, 03), MpaaRating = MpaaRating.PG13, Genre = "Adventure", Rating = 7 },
+                new Movie { Id = 2, Name = "Beauty and the Beast", ReleaseDate = new DateTime(2017, 03, 17), MpaaRating = MpaaRating.PG13, Genre = "Family", Rating = 7.8 },
+                new Movie { Id = 3, Name = "The Secret Life of Pets", ReleaseDate = new DateTime(2016, 07, 08), MpaaRating = MpaaRating.G, Genre = "Adventure", Rating = 6.6 },
+                new Movie { Id = 4, Name = "The Jungle Book", ReleaseDate = new DateTime(2016, 04, 15), MpaaRating = MpaaRating.PG, Genre = "Fantasy", Rating = 7.5 },
+                new Movie { Id = 5, Name = "Split", ReleaseDate = new DateTime(2017, 01, 20), MpaaRating = MpaaRating.PG13, Genre = "Horror", Rating = 7.4 },
+                new Movie { Id = 6, Name = "The Mummy", ReleaseDate = new DateTime(2017, 06, 09), MpaaRating = MpaaRating.R, Genre = "Action", Rating = 6.7 },
+            };
+        }
+
+        public static IEnumerable<Movie> GetList(bool forKidsOnly, double minimumRating)
+        {
+            return GetList().Where(x => (x.MpaaRating <= MpaaRating.PG || !forKidsOnly)
+            && x.Rating >= minimumRating).ToList();
+        }
+        
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using NStack;
+using Terminal.Gui;
+using TestingGuiCS.Models;
+
+namespace TestingGuiCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init ();
+            var top = Application.Top;
+
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar (new MenuBarItem [] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_Quit", "", () => { top.Running = false; })
+                })
+            });
+            top.Add (menu);
+
+            
+            // Creates the top-level window to show
+            var win = new Window (new Rect (0, 1, top.Frame.Width, top.Frame.Height-1), "Movie Db");
+            top.Add (win);
+
+            // Add some controls
+            var txtSearchLbl = new Label(3, 1, "Movie Name: ");
+            var txtSearch = new TextField(15, 1, 30, "");
+            var forKidsOnly = new CheckBox(3, 3, "For Kids?");
+            var minimumRatingLbl = new Label(25, 3, "Minimum Rating: ");
+            var minimumRatingTxt = new TextField(41, 3, 10, "");
+            var searchBtn = new Button(3, 5, "Filter");
+            var allMoviesListView = new ListView(new Rect(4, 8, top.Frame.Width, 200), MovieDataSource.GetList(forKidsOnly.Checked, 0).ToList());
+            searchBtn.Clicked = () =>
+            {
+                
+                double rating = 0;
+                var isDouble = double.TryParse(minimumRatingTxt.Text.ToString(), out rating);
+                if(!string.IsNullOrEmpty(minimumRatingTxt.Text.ToString()) && !isDouble)
+                {
+                    MessageBox.ErrorQuery(30, 6, "Error", "Rating must be number");
+                    minimumRatingTxt.Text = ustring.Empty;
+                    return;
+                }
+                
+                win.Remove(allMoviesListView);
+                if (string.IsNullOrEmpty(txtSearch.Text.ToString()) || string.IsNullOrEmpty(minimumRatingTxt.Text.ToString()))
+                {
+                    allMoviesListView = new ListView(new Rect(4, 8, top.Frame.Width, 200),
+                        MovieDataSource.GetList(forKidsOnly.Checked, rating).ToList());
+                    win.Add(allMoviesListView);
+                }
+                else
+                {
+                    win.Remove(allMoviesListView);
+                    win.Add(new ListView(new Rect(4, 8, top.Frame.Width, 200), 
+                        MovieDataSource.GetList(forKidsOnly.Checked, rating)
+                            .Where(x => 
+                            x.Name.Contains(txtSearch.Text.ToString(), StringComparison.OrdinalIgnoreCase)
+                        ).ToList()));
+                }
+                
+            };
+            win.Add (
+                txtSearchLbl,
+                txtSearch,
+                forKidsOnly,
+                minimumRatingLbl,
+                minimumRatingTxt,
+                searchBtn,
+                new Label (3, 7, "-------------Search Result--------------")
+            );
+
+            Application.Run ();
+        }
+    }
+}
