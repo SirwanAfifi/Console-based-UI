0444d95519da2486603418eabe88070988f39715
Sirwan Afifi <sirwan@Sirwans-MacBook-Pro.local>
2018-05-11 11:26:40 +0430

    added generic specification

---
+++ Core/GenericSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace TestingGuiCS.Core
+{
+    public class GenericSpecification<T>
+    {
+        public Expression<Func<T, bool>> Expression;
+
+        public GenericSpecification(Expression<Func<T, bool>> expression)
+        {
+            Expression = expression;
+        }
+
+        public bool IsSatisifiedBy(T entity)
+        {
+            return Expression.Compile().Invoke(entity);
+        }
+    }
+}
